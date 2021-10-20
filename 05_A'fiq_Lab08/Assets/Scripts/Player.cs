0691b324a9dec90b5cbad2e39f1ef67e29dc0466
05-Afiq <91868671+05-Afiq@users.noreply.github.com>
2021-10-20 10:18:20 +0800

    Updated Random

---
+++ 05_A'fiq_Lab08/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+        Vector3 pos = transform.position + new Vector3(0, verticalInput * speed * Time.deltaTime, 0);
+        pos.y = Mathf.Clamp(transform.position.y, -4, 4);
+        transform.position = pos;
+
+
+
+    }
+}
---
+++ 05_A'fiq_Lab08/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    float PositionY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        InvokeRepeating("SpawnObjects", 1, 1);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        int prefeb_num = Random.Range(0, prefabs.Length);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        Instantiate(prefabs[prefeb_num], transform.position, transform.rotation);
+    }
+}
